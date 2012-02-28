d2aa507e19de0860ff3680d5941e2577202350fc
Sho Hashimoto <hashimoto@shokai.org>
2012-02-28 21:04:15 +0900

    use tempfile in POST /file

--- controllers/print.rb
+++ controllers/print.rb
@@ -4,32 +4,26 @@ get '/list.json' do
 end
 
 post '/file' do
-  if !params[:file] or !params[:printer]
+  if !params[:file] or !params[:file][:tempfile] or !params[:printer]
     status 400
     @mes = 'bad request'
     haml :message
   else
-    data = params[:file][:tempfile].read
-    Dir.mkdir filedir unless File.exists? filedir
-    fname = "#{filedir}/#{Digest::MD5.hexdigest data}#{File.extname params[:file][:filename]}"
-    File.open(fname, 'wb') do |f|
-      f.write data
-    end
-    unless File::exists? fname
-      status 500
-      @mes = 'file upload error'
+    f = Tempfile.new('print-server')
+    begin
+      f.write params[:file][:tempfile].read
+      pr = Printer.new(params[:printer])
+      pr.print(fname) unless @@conf['no_print']
+      @mes = 'printing!!'
       haml :message
-    else
-      begin
-        pr = Printer.new(params[:printer])
-        pr.print(fname) unless @@conf['no_print']
-        @mes = 'printing!!'
-        haml :message
-      rescue => e
-        STDERR.puts e
-        @mes = e.to_s
-        haml :message
-      end
+    rescue => e
+      STDERR.puts e
+      status 500
+      @mes = e.to_s
+      haml :message
+    ensure
+      f.close
+      f.delete
     end
   end
 end
