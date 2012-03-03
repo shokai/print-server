0d02ae54eec23001ce71085f3f8038e734f5a2a4
Sho Hashimoto <hashimoto@shokai.org>
2012-03-04 00:05:07 +0900

    use mini_magick, support landscape layout PDF

--- helpers/web_capture.rb
+++ helpers/web_capture.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 ## capture webpage and make PDF
-## brew install capybara capybara-webkit headless
+## brew install qt imagemagick
+## gem install capybara capybara-webkit headless prawn mini_magick
 
 require 'rubygems'
 require 'capybara-webkit'
 require 'headless'
 require 'tmpdir'
 require 'prawn'
+require 'mini_magick'
 
 class WebCapture
   class Error < StandardError
@@ -22,24 +24,25 @@ class WebCapture
       end
       
       raise Error.new "capture failed" unless File.exists? png
-
-      x,y = `identify '#{png}'`.split(/\s/).select{|i|
-        i =~ /^\d+x\d+$/
-      }.first.split('x').map{|i| i.to_i}
       
-      w = x
+      img = MiniMagick::Image.open png
+      w = img[:width]
       h = params[:landscape] ? (w/1.41).to_i : (w*1.41).to_i
       
-      parts = 0.upto(y/h).map{|i|
+      parts = 0.upto(img[:height]/h).map{|i|
         fname = "#{dir}/#{i}.jpg"
-        puts cmd = "convert -quality 100 -crop #{w}x#{h}+0+#{h*i} '#{png}' '#{fname}'"
-        system cmd
+        img = MiniMagick::Image.open png
+        img.crop("#{w}x#{h}+0+#{i*h}")
+        img.write fname
         fname
       }
       
-      Prawn::Document.generate(params[:out]) do
-        parts.each do |img|
-          text params[:url]
+      Prawn::Document.generate(params[:out],
+                               :page_layout => params[:landscape] ? :landscape : :portrait) do
+        for i in 0...(parts.size) do
+          img = parts[i]
+          start_new_page if i > 0
+          text "#{params[:url]} [#{i+1}/#{parts.size}]"
           image img, :fit => [bounds.width, bounds.height-20]
         end
       end
